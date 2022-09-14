61add82a2a21414415be49bc61f4072cc317d426
bombag456 <bombag456@gmail.com>
2022-09-14 16:09:42 +0600

    kjfe

--- homework1/main.swift
+++ homework1/main.swift
@@ -7,8 +7,9 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Первое задание!")
 
+            //2
 func solo(num: Int) ->Int{
     print("Квадрат \(num) = \(num*num)")
     return num*num
@@ -17,8 +18,8 @@ let a = solo(num: 2)
 
 
 func solo2(num1: Int) -> Int{
-    print("Куб числа \(num1) = \(num1*num1)")
-    return num1*num1
+    print("Куб числа \(num1) = \(num1*num1*num1)")   //27
+    return num1*num1*num1
 }
 let b = solo2(num1: 3)
 
@@ -31,9 +32,77 @@ func solo3(num3:Int) ->Int{
 }
 let f = solo3(num3: 5)
 
-print("S=чило.пи*R")
-func solo23(num4:Double) ->Double{
-    print("(Радиус круга =10 число пи=3.14) 10*\(num4) = \(num4*num4)")
-    return num4+num4
+print("S=чило.PI*R")
+
+func solo222(num23:Double){
+    print("Площадь круга 9*\(num23) = \(num23*9)")
 }
-let g = solo23(num4: 3.14)
+
+let k = solo222(num23: 3.14)
+
+//28.26
+
+print("Второе задание!")
+  
+
+
+print("*")
+
+func add1(num42 :Int) {
+    print ( "Скoлько будет 28*\(num42) = \(num42*28)")
+}
+        
+let a2 = add1(num42: 78)
+
+
+print("+")
+func add2(num44 :Double){
+    print("2.3+\(num44) = \(num44+2.3)")
+}
+let a3 = add2(num44: 4.2)
+
+print("-")
+
+func add3(num45 : Int){
+    print("78-\(num45)=\(num45-78)")
+}
+let a4 = add3(num45: 100)
+
+print("/")
+
+func add5(num56 :Int){
+    print("3/\(num56)=\(num56/3)")
+}
+let f3 = add5(num56: 9)
+
+
+func add6(num66 :Double){
+    print("-7.14*\(num66)=\(num66 * -7.14)")
+}
+let f4 = add6(num66: 14)
+//-99.96 ответ!
+
+
+func add(num77:Int){
+    print("-256-\(num77)=\(num77-256)")
+}
+let f5 = add(num77: 200)
+//ответ -56
+
+func add8(num88:Int) -> Int{
+    print("25*\(num88)=\(num88*num88)")
+          return num88*num88
+}
+let f6 = add8(num88: 25)
+//ответ 625
+
+func add98(num99:Int){
+    print("3/\(num99)=\(num99/3)")
+}
+let hj = add98(num99: 34)
+//ответ 11
+
+func add9(num102:Int){
+    print("19-\(num102)=\(num102-19)")
+}
+let f8 = add9(num102: 19)
